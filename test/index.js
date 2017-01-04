27fdcf53fc0a2b69ebd7acde6c43bb3badfd6791
Daniel Cousens <github@dcousens.com>
2017-01-04 11:20:36 +1100

    tests: use an existing method name

--- test/index.js
+++ test/index.js
@@ -2,9 +2,11 @@ let Yajrpc = require('../')
 let rpc = new Yajrpc({
   url: 'http://localhost:8332',
   user: 'bitcoinrpc',
+
+  // 'typical' password
   pass: '5BHFp1QgcNsYtUNq7pu3zDbk5gFZFSwmy32ky7q3z9Fy'
 })
 
-rpc.call('func1', [1, 2, 3], (err, result) => {
+rpc.call('getbestblockhash', [], (err, result) => {
   console.log(err, result)
 })
